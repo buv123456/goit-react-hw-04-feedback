bb05f505bd76f2874d8f68c9806cbb393fc4d6cf
Volodymyr <buvoro@gmail.com>
2023-09-11 15:14:58 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
-export class App extends Component {
-  state = {
+export function App() {
+  const [marks, setMarks] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const handleOnClick = name => {
+    setMarks(prev => ({ ...prev, [name]: prev[name] + 1 }));
   };
 
-  handleOnClick = name => {
-    this.setState(prev => ({ [name]: prev[name] + 1 }));
-  };
+  const totalFeedback = Object.values(marks).reduce((a, i) => a + i, 0);
+  const positiveFeedbackPercentage = (
+    (marks.good * 100) /
+    totalFeedback
+  ).toFixed(0);
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((a, i) => a + i, 0);
-
-  countPositiveFeedbackPercentage = () =>
-    ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0);
-
-  render() {
-    return (
-      <Section title='What do you think about our "EXpresso"?'>
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.handleOnClick}
+  return (
+    <Section title='What do you think about our "EXpresso"?'>
+      <FeedbackOptions
+        options={Object.keys(marks)}
+        onLeaveFeedback={handleOnClick}
+      />
+      {totalFeedback ? (
+        <Statistics
+          entries={Object.entries(marks)}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage}
         />
-        {this.countTotalFeedback() ? (
-          <Statistics
-            entries={Object.entries(this.state)}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <Notification message="There is no feedback yet!" />
-        )}
-      </Section>
-    );
-  }
+      ) : (
+        <Notification message="There is no feedback yet!" />
+      )}
+    </Section>
+  );
 }
